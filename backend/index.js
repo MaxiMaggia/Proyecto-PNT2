59a23bdf844437244acfaf6b4e0a161abae2102a
MaxiMaggia <maximomaggiaiuolo@gmail.com>
2025-11-14 14:36:54 -0300

    Eliminar carpeta backend completamente del repositorio

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-// Punto de entrada: levanta el Server con la config cargada.
-import Server from './server.js';
-import config from './config.js';
-
-const server = new Server(config.PORT);
-server.start();
--- backend/middleware/cors.js
+++
@@ -1,8 +0,0 @@
-// Habilita CORS para permitir que el frontend (Expo) consuma la API.
-import cors from 'cors';
-import config from '../config.js';
-
-export default cors({
-  origin: config.CORS_ORIGIN === '*' ? true : config.CORS_ORIGIN,
-  credentials: false
-});
